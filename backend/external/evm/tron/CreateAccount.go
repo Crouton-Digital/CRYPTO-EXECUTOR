47a9a9a25a6906a46958f405487175ba72dd1776
Google Cloud Build <f6b9g8wdbqehaq@estchange.io>
2023-04-10 16:48:31 +0400

    Rename Create Account

--- backend/external/evm/tron/CreateAccount.go
+++ backend/external/evm/tron/CreateAccount.go
@@ -3,11 +3,19 @@ package tron
 import (
 	"fmt"
 	"github.com/fbsobreira/gotron-sdk/pkg/account"
+	"github.com/fbsobreira/gotron-sdk/pkg/client"
 	"github.com/fbsobreira/gotron-sdk/pkg/store"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 )
 
-func TronCreateAccount() {
+func CreateAccount() {
+
+	conn := client.NewGrpcClient("grpc.trongrid.io:50051")
+	err := conn.Start(grpc.WithInsecure())
+	if err != nil {
+		logrus.Errorf("Error connecting GRPC Client: %v", err)
+	}
 
 	acc := &account.Creation{
 		Name:       "maxiWallet",
